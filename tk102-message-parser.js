d6cdac9a344bee027bdf4f76ab01aa26a4fc970c
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-07-12 19:45:22 -0300

    Implementando o tratamento do número de celular e IMEI.

--- tk102-message-parser.js
+++ tk102-message-parser.js
@@ -2,28 +2,28 @@ module.exports = function(RED) {
     "use strict";
 
     function fixGeo(one, two) {
-      var minutes = one.substr (-7, 7);
-      var degrees = parseInt (one.replace (minutes, ''), 10);
+      var minutes = one.substr(-7, 7);
+      var degrees = parseInt(one.replace(minutes, ''), 10);
 
-      one = degrees + (minutes / 60);
-      one = parseFloat ((two === 'S' || two === 'W' ? '-' : '') + one);
+      one = degrees +(minutes / 60);
+      one = parseFloat((two === 'S' || two === 'W' ? '-' : '') + one);
 
-      return Math.round (one * 1000000) / 1000000;
+      return Math.round(one * 1000000) / 1000000;
     };
 
     function checksum(raw) {
-      var str = raw.trim () .split (/[,*#]/);
-      var strsum = parseInt (str [15], 10);
-      var strchk = str.slice (2, 15) .join (',');
+      var str = raw.trim() .split(/[,*#]/);
+      var strsum = parseInt(str[15], 10);
+      var strchk = str.slice(2, 15) .join(',');
       var check = 0;
       var i;
 
-      for (i = 0; i < strchk.length; i++) {
-        check ^= strchk.charCodeAt (i);
+      for(i = 0; i < strchk.length; i++) {
+        check ^= strchk.charCodeAt(i);
       }
 
-      check = parseInt (check.toString (16), 10);
-      return (check === strsum);
+      check = parseInt(check.toString(16), 10);
+      return(check === strsum);
     };
 
     function parseMessage(raw) {
@@ -34,47 +34,47 @@ module.exports = function(RED) {
       var gpstime = '';
 
       try {
-        raw = raw.trim ();
-        str = raw.split (',');
+        raw = raw.trim();
+        str = raw.split(',');
 
-        if (str.length === 18 && str [2] === 'GPRMC' || str.length === 28 && str [2] === 'GPRMC') {
-          datetime = str [0] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function (s, y, m, d, h, i) {
+        if(str.length === 18 && str[2] === 'GPRMC' || str.length === 28 && str[2] === 'GPRMC') {
+          datetime = str[0].replace(/([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function(s, y, m, d, h, i) {
             return '20' + y + '-' + m + '-' + d + ' ' + h + ':' + i;
           });
 
-          gpsdate = str [11] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})/, function (s, d, m, y) {
+          gpsdate = str[11].replace(/([0-9]{2})([0-9]{2})([0-9]{2})/, function(s, d, m, y) {
             return '20' + y + '-' + m + '-' + d;
           });
 
-          gpstime = str [3] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})\.([0-9]{3})/, function (s0, h, i, s, ms) {
+          gpstime = str[3].replace(/([0-9]{2})([0-9]{2})([0-9]{2})\.([0-9]{3})/, function(s0, h, i, s, ms) {
             return h + ':' + i + ':' + s + '.' + ms;
           });
 
           result = {
             raw: raw,
             datetime: datetime,
-            phone: str [1],
+            phone: str[1],
             gps: {
               date: gpsdate,
               time: gpstime,
-              signal: str [15] === 'F' ? 'full' : 'low',
-              fix: str [4] === 'A' ? 'active' : 'invalid'
+              signal: str[15] === 'F' ? 'full' : 'low',
+              fix: str[4] === 'A' ? 'active' : 'invalid'
             },
             geo: {
-              latitude: fixGeo (str [5], str [6]),
-              longitude: fixGeo (str [7], str [8]),
-              bearing: parseInt (str [10], 10)
+              latitude: fixGeo(str[5], str[6]),
+              longitude: fixGeo(str[7], str[8]),
+              bearing: parseInt(str[10], 10)
             },
             speed: {
-              knots: Math.round (str [9] * 1000) / 1000,
-              kmh: Math.round (str [9] * 1.852 * 1000) / 1000,
-              mph: Math.round (str [9] * 1.151 * 1000) / 1000
+              knots: Math.round(str[9] * 1000) / 1000,
+              kmh: Math.round(str[9] * 1.852 * 1000) / 1000,
+              mph: Math.round(str[9] * 1.151 * 1000) / 1000
             },
-            imei: str [16] .replace ('imei:', ''),
-            checksum: checksum (raw)
+            imei: str[17].replace('imei:', '').trim(),
+            checksum: checksum(raw)
           };
         }
-      } catch (e) {
+      } catch(e) {
         result = null;
       }
 
@@ -85,9 +85,16 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         var node = this;
 
+        this.cellphone = config.cellphone;
+        this.imei = config.imei;
+
         this.on("input", function(msg) {
             msg.payload = parseMessage(msg.payload);
-            node.send(msg);
+
+            if (this.cellphone == msg.payload.phone || this.imei == msg.payload.imei)
+              node.send(msg);
+            else
+              node.error("Atenção: O número de celular ou IMEI recebidos na mensagem não bate(m) com o informado na configuração.", msg);
         });
     }
 
